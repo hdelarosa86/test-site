4f0577b4984390a82d59f713250f140df176a052
Hector DelaRosa <hdelaros@gmail.com>
2022-06-03 23:15:57 -0400

    modified logging:false

--- server/db/database.js
+++ server/db/database.js
@@ -6,7 +6,7 @@ const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
   {
-    logging: true,
+    logging: false,
   }
 );
 
