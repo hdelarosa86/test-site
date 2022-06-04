d3db368b4582a69ee2b651ab05cc37d29c5dd837
Hector DelaRosa <hdelaros@gmail.com>
2022-06-03 22:56:26 -0400

    db debug

--- server/db/database.js
+++ server/db/database.js
@@ -3,12 +3,9 @@ const pkg = require('../../package.json');
 const colors = require('colors');
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
-  {
-    logging: false,
-  }
-);
+const db = new Sequelize(process.env.DATABASE_URL, {
+  logging: true,
+});
 
 const authenticateDB = async () => {
   try {
